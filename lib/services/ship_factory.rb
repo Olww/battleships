d59c2f243cda4dfd1211969cf00ecb3fe3d12765
Rodion Radchenko <rodion.radchenko@gmail.com>
2023-06-01 14:17:13 +0200

    Add ship factory

---
+++ lib/services/ship_factory.rb
@@ -0,0 +1,18 @@
+require_relative '../models/ship'
+
+class ShipFactory
+  def self.build_ship(length:, start_coordinate:, end_coordinate:)
+    case length
+    when 1
+      Ship.create_drone_boat(start_coordinate: start_coordinate, end_coordinate: end_coordinate)
+    when 2
+      Ship.create_patrol_boat(start_coordinate: start_coordinate, end_coordinate: end_coordinate)
+    when 3
+      Ship.create_submarine(start_coordinate: start_coordinate, end_coordinate: end_coordinate)
+    when 4
+      Ship.create_destroyer(start_coordinate: start_coordinate, end_coordinate: end_coordinate)
+    else
+      raise ArgumentError.new("Invalid ship length: #{length}")
+    end
+  end
+end
